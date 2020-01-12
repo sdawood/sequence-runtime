047b2f1cd791a2191328d73d555ce37b666ed441
shaadydawood <shaadydawood@gmail.com>
2020-01-12 23:54:06 +0200

    redux-store-dual using pipe and scan

--- src/core/pipe.spec.js
+++ src/core/pipe.spec.js
@@ -174,6 +174,229 @@ describe('scenario: pipe receiving a nested pipe', () => {
     ];
     expect(result).toEqual(expectedResult);
   });
+
+  it('works: like a redux store, unhandled actions are yielded through!!', () => {
+    const initialState = {state: 0};
+    const ofType = actionType => ({type, payload}) => type === actionType ? payload : F.reduced({type, payload});
+
+    const store = pipe(
+      ofType('input'),
+      n => n * 2,
+      n => range({start: 1, end: n + 1}),
+      pipe(
+        n => n * 10
+      ),
+      iter => scan((acc, input) => ({...acc, state: acc.state + input}), () => initialState, iter, ({state}) => state),
+      transformedGen => [...transformedGen],
+      // result => result.slice(-1),
+    );
+
+    const actions = ([
+      {type: 'input', payload: 1},
+      {type: 'something-else', payload: Infinity},
+      {type: 'input', payload: 2},
+      {type: 'input', payload: 3},
+      {type: 'something-else', payload: Infinity},
+      {type: 'input', payload: 4},
+    ]);
+
+    const stateHistory = [...store(actions)];
+
+    const expectedResult = [
+      [
+        {
+          "state": 10
+        },
+        {
+          "state": 30
+        },
+        30
+      ],
+      {
+        "payload": Infinity,
+        "type": "something-else"
+      },
+      [
+        {
+          "state": 10
+        },
+        {
+          "state": 30
+        },
+        {
+          "state": 60
+        },
+        {
+          "state": 100
+        },
+        100
+      ],
+      [
+        {
+          "state": 10
+        },
+        {
+          "state": 30
+        },
+        {
+          "state": 60
+        },
+        {
+          "state": 100
+        },
+        {
+          "state": 150
+        },
+        {
+          "state": 210
+        },
+        210
+      ],
+      {
+        "payload": Infinity,
+        "type": "something-else"
+      },
+      [
+        {
+          "state": 10
+        },
+        {
+          "state": 30
+        },
+        {
+          "state": 60
+        },
+        {
+          "state": 100
+        },
+        {
+          "state": 150
+        },
+        {
+          "state": 210
+        },
+        {
+          "state": 280
+        },
+        {
+          "state": 360
+        },
+        360
+      ]
+    ];
+    expect(stateHistory).toEqual(expectedResult);
+  });
+
+  it('works: unhandled actions are yielded through and can be handled in a follow up pipe', () => {
+    const initialState = {state: 0};
+    const ofType = actionType => action => action.type === actionType ? action.payload : F.reduced(action);
+
+    const store = pipe(
+      ofType('input'),
+      n => n * 2,
+      n => range({start: 1, end: n + 1}),
+      pipe(
+        n => n * 10
+      ),
+      iter => scan((acc, input) => ({...acc, state: acc.state + input}), () => initialState, iter, ({state}) => state),
+      transformedGen => [...transformedGen],
+      // result => result.slice(-1),
+    );
+
+    const actions = ([
+      {type: 'input', payload: 1},
+      {type: 'something-else', payload: Infinity},
+      {type: 'input', payload: 2},
+      {type: 'input', payload: 3},
+      {type: 'something-else', payload: Infinity},
+      {type: 'input', payload: 4},
+    ]);
+
+    const stateHistory = [
+      ...pipe(
+        ofType('something-else'),
+        unhandledAction => `rejected action of type: ${unhandledAction.type}`
+    )(store(actions))
+    ];
+
+    const expectedResult = [
+      [
+        {
+          "state": 10
+        },
+        {
+          "state": 30
+        },
+        30
+      ],
+      "rejected action of type: undefined",
+      [
+        {
+          "state": 10
+        },
+        {
+          "state": 30
+        },
+        {
+          "state": 60
+        },
+        {
+          "state": 100
+        },
+        100
+      ],
+      [
+        {
+          "state": 10
+        },
+        {
+          "state": 30
+        },
+        {
+          "state": 60
+        },
+        {
+          "state": 100
+        },
+        {
+          "state": 150
+        },
+        {
+          "state": 210
+        },
+        210
+      ],
+      "rejected action of type: undefined",
+      [
+        {
+          "state": 10
+        },
+        {
+          "state": 30
+        },
+        {
+          "state": 60
+        },
+        {
+          "state": 100
+        },
+        {
+          "state": 150
+        },
+        {
+          "state": 210
+        },
+        {
+          "state": 280
+        },
+        {
+          "state": 360
+        },
+        360
+      ]
+    ];
+    expect(stateHistory).toEqual(expectedResult);
+  });
 });
 
 describe('scenario: pipeWith(...pipelineHigherOrderFns)(ctx)', () => {
@@ -190,6 +413,22 @@ describe('scenario: pipeWith(...pipelineHigherOrderFns)(ctx)', () => {
       "21",
       "31",
       "41"
+    ];
+    expect([...result]).toEqual(expectedResult);
+  });
+
+  it('works: pipeline functions can accept the ctx themselves for specialized interaction', () => {
+    const strIntStr = {preprocess: str => parseInt(str, 10), postprocess: n => `${n}`};
+
+    const result = pipeWith(strIntStr)(
+      ctx => n => ctx.postprocess(ctx.preprocess(n) * 10),
+      ctx => n => `result: ${ctx.preprocess(n) + 1}`,
+    )(['1', '2', '3', '4']);
+    const expectedResult = [
+      "result: 11",
+      "result: 21",
+      "result: 31",
+      "result: 41"
     ];
     expect([...result]).toEqual(expectedResult);
   });
